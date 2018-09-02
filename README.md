4e129f914c90db93b97fafd977c48d44a32d3bbc
Raymond van der Straten <raymond.1993@hotmail.com>
2018-09-02 15:17:41 +0200

    First usable package publish by @levarne

---
+++ README.md
@@ -0,0 +1,115 @@
+# AWSLambdaHelper
+AWS Lambda helper for default functions(e.g. time logging, event/context logging and creating callback object for AWS API Gateway)
+
+This library is designed to work with AWS API Gateway with lambda proxy integration. It is created for handling the most basic functions, like checking required parameters and sending error logs to an AWS CloudWatch Log Group.
+
+## Installation
+
+```
+$ npm install @levarne/awslambdahelper
+
+const awsLambdaHelper = require('@levarne/awslambdahelper');
+```
+
+## Usage
+If you want the library to check for required header/body parameters, set the correct environment variables.
+
+For header params: ``REQUIRED_HEADER_PARAMS=myRequiredHeaderParam1,myRequiredHeaderParam2``
+
+For body params: ``REQUIRED_BODY_PARAMS=myRequiredBodyParam1,myRequiredBodyParam2``
+
+If you want to use the logError method to send error logs to a cloudwatch log group, set the cloudwatch log group name in your environment variables: ``CW_LOG_GROUP_NAME=myLogGroupName``
+
+Currently, there are 5 available functions:
+
+**Init method:**
+
+```
+awsLambdaHelper.init(event, context, callback);
+```
+
+The init function will parse the event.body(if any body is available and the available body is a string). It will also log the event as well as the context. Furthermore, this will start tracking time. If there is a password key in the body, this will be logged as *****. If there are required header and/or body parameters available in the environment variables, the init function will check if those parameters are send with the request. **If you use the init method, always use the callbackResponse() method for your lambda function.**
+
+**Log error method:**
+
+```
+awsLambdaHelper.logError(myError);
+```
+
+This will send a log object to the log group, below is an example where an AWS service responded with an error, this error is converted to a complete error message:
+
+```
+{
+  "message": "Requested resource not found",
+  "code": "ResourceNotFoundException",
+  "time": "2018-08-31T12:34:02.038Z",
+  "requestId": "VE4NC0MV96RDQNTIRSF8SF4UHFVV4KQNSO5AEMVJF66Q9ASUAAJG",
+  "statusCode": 400,
+  "retryable": false,
+  "retryDelay": 17.04691844117645,
+  "level": "ERROR",
+  "functionName": "local",
+  "event": {
+    "body": {
+      "echo": "foobar"
+    }
+  },
+  "context": {
+    "local": true
+  }
+}
+```
+
+**Callback response method:**
+
+```
+awsLambdaHelper.callbackResponse(statusCode, body, event, context, callback);
+```
+
+The callback response method will return a correct API Gateway response. It will also log the response, finish time tracking and send any available logs to cloudwatch.
+
+**Get environment method:**
+
+```
+awsLambdaHelper.getEnvironment();
+```
+
+If the function is deployed, this will return your stage(e.g. if your function is named "get_user-development", it will return "development"). If the function is running locally, and no AWS_ENVIRONMENT is available in the environment variables, this will return "local".
+
+**Get function name method:**
+
+```
+awsLambdaHelper.getFunctionName();
+```
+
+If the function is deployed, this will return the full function name. If your function is running locally, it will return "local-" followed by the environment variable AWS_ENVIRONMENT. If no AWS_ENVIRONMENT is available in the environment variables, the get function name method will return "local-local".  
+
+## Simple example
+
+```
+const async = require('async');
+const awsLambdaHelper = require('@levarne/awslambdahelper');
+
+exports.handler = function(event, context, callback) {
+
+  async.waterfall([
+    init = function(callback) {
+      awsLambdaHelper.init(event, context, callback);
+    },
+    doEcho = function(callback) {
+      echo(event.body.echo, callback);
+    }
+  ], function (err, result) {
+    if (err) {
+      awsLambdaHelper.callbackResponse(err.statusCode, err, callback);
+    } else {
+      awsLambdaHelper.callbackResponse(200, result, callback);
+    }
+  });
+
+}
+
+var echo = function(echo, callback) {
+  callback(undefined, echo);
+}
+```
---
+++ index.js
@@ -0,0 +1,219 @@
+const AWSXRay = require('aws-xray-sdk');
+const AWS = AWSXRay.captureAWS(require('aws-sdk'));
+const async = require('async');
+const uuidv4 = require('uuid/v4');
+const cloudwatchlogs = new AWS.CloudWatchLogs();
+
+const functionName = process.env.AWS_LAMBDA_FUNCTION_NAME ? process.env.AWS_LAMBDA_FUNCTION_NAME : 'local-' + (process.env.AWS_ENVIRONMENT ? process.env.AWS_ENVIRONMENT : 'local');
+const environment = functionName.split('-')[1];
+const requiredHeaderParams = process.env.REQUIRED_HEADER_PARAMS ? process.env.REQUIRED_HEADER_PARAMS.split(',') : [];
+const requiredBodyParams = process.env.REQUIRED_BODY_PARAMS ? process.env.REQUIRED_BODY_PARAMS.split(',') : [];
+const logGroupName = process.env.CW_LOG_GROUP_NAME ? process.env.CW_LOG_GROUP_NAME : undefined;
+
+var logEvent;
+var logContext;
+var logMessages;
+
+module.exports = {
+  init: function(event, context, callback) {
+    console.time(functionName);
+
+    if (event.body && typeof event.body === 'string') {
+      event.body = JSON.parse(event.body);
+    }
+
+    logEvent = JSON.parse(JSON.stringify(event));
+    if (logEvent.body && logEvent.body.password) {
+      logEvent.body.password = '*****';
+    }
+    logContext = context;
+    logMessages = [];
+
+    console.info('Received event:', JSON.stringify(logEvent, null, 2));
+    console.info('Received context:', JSON.stringify(logContext, null, 2));
+
+    checkRequiredParams(event.headers, event.body, callback)
+  },
+
+  callbackResponse: function(statusCode, body, callback) {
+    var response = {
+      statusCode: statusCode,
+      headers: {
+        "Access-Control-Allow-Origin" : "*",
+        "Access-Control-Allow-Credentials" : true
+      },
+      body: JSON.stringify(body)
+    }
+
+    console.timeEnd(functionName);
+
+    if (statusCode >= 500) {
+      callback(JSON.stringify(response));
+    } else {
+      callback(undefined, response);
+    }
+
+    postMessages();
+  },
+
+  logError: function(error, options) {
+    if (!error.time) {
+      error.time = new Date().toISOString()
+    }
+
+    var logMessage = {
+      level: 'ERROR',
+      time: error.time,
+      functionName: functionName
+    }
+
+    logMessage = Object.assign({}, error, logMessage);
+
+    if (options) {
+      logMessage = Object.assign({}, options, logMessage);
+    }
+
+    logMessages.push(logMessage);
+  },
+
+  getEnvironment: function() {
+    return environment;
+  },
+
+  getFunctionName: function() {
+    return functionName;
+  }
+}
+
+var checkRequiredParams = function(headers, body, callback) {
+  async.parallel({
+    headerParams: function(callback) {
+      checkHeaderParams(headers, callback);
+    },
+    bodyParams: function(callback) {
+      checkBodyParams(body, callback);
+    }
+  }, function(err, results) {
+    if (err) {
+      callback(err);
+    } else {
+      callback();
+    }
+  });
+}
+
+var checkHeaderParams = function(headers, callback) {
+  if (requiredHeaderParams.length === 0 || (requiredHeaderParams.length === 1 && requiredHeaderParams[0] === '')) {
+    callback();
+  } else {
+    if (!headers) {
+      var error = {
+        statusCode: 'InvalidParameterException',
+        message: 'No headers found in the request',
+        statusCode: 400
+      }
+      callback(error);
+    } else {
+      checkValues(requiredHeaderParams, headers, callback);
+    }
+  }
+}
+
+var checkBodyParams = function(body, callback) {
+  if (requiredBodyParams.length === 0 || (requiredBodyParams.length === 1 && requiredBodyParams[0] === '')) {
+    callback();
+  } else {
+    if (!body) {
+      var error = {
+        statusCode: 'InvalidParameterException',
+        message: 'No body found in the request',
+        statusCode: 400
+      }
+      callback(error);
+    } else {
+      checkValues(requiredBodyParams, body, callback);
+    }
+  }
+}
+
+var checkValues = function(params, values, callback) {
+  async.each(params, function(param, callback) {
+    if(!isValue(values[param])) {
+      var error = {
+        statusCode: 'InvalidParameterException',
+        message: param + ' is required',
+        statusCode: 400
+      }
+      callback(error);
+    } else {
+      callback();
+    }
+  }, callback);
+}
+
+var isValue = function(value) {
+  if ((!value && value !== false && value !== 0) || value === '') {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+var postMessages = function() {
+  if (logMessages.length > 0) {
+    if (!logGroupName) {
+      console.log('No log group available in environment variables, not posting messages');
+      console.log('Messages received for logging:');
+      console.log(JSON.stringify(logMessages, null, 2));
+    } else {
+      var logEvents = [];
+      async.each(logMessages, function(logMessage, callback) {
+        logMessage.event = logEvent;
+        logMessage.context = logContext;
+        logEvents.push({
+          message: JSON.stringify(logMessage, null, 2),
+          timestamp: new Date(logMessage.time).getTime()
+        });
+        callback();
+      }, function(err) {
+        if (err) {
+          console.log('An error occurred processing logmessages');
+        } else {
+          async.waterfall([
+            createLogStream = function(callback) {
+              var params = {
+                logGroupName: logGroupName,
+                logStreamName: functionName + '/' + uuidv4()
+              }
+              cloudwatchlogs.createLogStream(params, function(err, result) {
+                if (err) {
+                  console.log('An error occured creating a new log stream');
+                  console.log(JSON.stringify(err, null, 2));
+                  callback(err);
+                } else {
+                  callback(undefined, params.logStreamName);
+                }
+              });
+            },
+            postMessages = function(logStreamName, callback) {
+              var params = {
+                logEvents: logEvents,
+                logGroupName: logGroupName,
+                logStreamName: logStreamName
+              }
+              cloudwatchlogs.putLogEvents(params, function(err, result) {
+                if (err) {
+                  console.log('An error occured posting log messages to stream');
+                  console.log(JSON.stringify(err, null, 2));
+                  callback(err);
+                } else {
+                  callback();
+                }
+              });
+            }
+          ]);
+        }
+      });
+    }
+  }
+}
